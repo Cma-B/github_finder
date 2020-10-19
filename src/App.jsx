9e4e5492fc3d855bebbc21e88a4f9370c48cdb4c
Sima <sima.boreiri@gmail.com>
2020-10-19 19:36:09 +0200

    adds axios to fetch external api from github, adds componentdidmount function to app file and fetch the users from github api, modifies the user component by changing class to function and remove hard coded data and passes the props from app to user

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import Navbar from "./components/layout /Navbar"
 import Users from './components/user/Users'
-
+import axios from 'axios'
 
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+}
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const response = await axios.get('https://api.github.com/users');
+    this.setState({ users: response.data, loading: false})
+  }
   render() {
     return (
       <div className='app'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
