4a3801126a7a5fc0786b3a2acd478e32e9c3ba19
Sima <sima.boreiri@gmail.com>
2020-10-19 20:53:02 +0200

    adds spinner gif and component of spinner to insert that spinner before full loading of the page

--- src/components/user/Users.jsx
+++ src/components/user/Users.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import UserItem from './UserItem'
+import UserItem from './UserItem';
+import Spinner from '../layout /Spinner.jsx';
+import PropTypes from 'prop-types'
 
-const Users = (props) => {
+const Users = ({ users, loading }) => {
 
-  
+  if (loading) {
+    return <Spinner />
+  } else {
     return (
-      <div style={userStyle}> {props.users.map(user => (
+      <div style={userStyle}> {users.map(user => (
         <UserItem key={user.id} user={user} />
       ))} </div>
     );
- 
+  }
 }
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+}
+
 
 const userStyle = {
   display: 'grid',
