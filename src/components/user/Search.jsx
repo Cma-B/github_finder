42fd91f0a96f76136f57549333b62b93ecfe93fa
Sima <sima.boreiri@gmail.com>
2020-10-21 22:00:02 +0200

    formatting code

--- src/components/user/Search.jsx
+++ src/components/user/Search.jsx
@@ -21,15 +21,16 @@ class Search extends Component {
     this.props.searchUsers(this.state.text)
     this.setState({ text: '' })
   }
-  
+
   render() {
+    const { showClear, clearUsers } = this.props
     return (
       <div>
         <form onSubmit={this.onSubmitHandler} className="form">
           <input type="text" name="text" placeholder="search Users..." value={this.state.text} onChange={this.onChangeHandler} />
           <input type="submit" value="Search" className="btn btn-dark btn-block" />
         </form>
-        {this.props.showClear && <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>}
+        {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
 
       </div>
     );
